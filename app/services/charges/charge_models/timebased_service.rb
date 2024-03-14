c448ee73b764dbaec8ec25e3e2d98d3de0fbb415
TronTram <txtron2012@gmail.com>
2024-03-14 16:32:42 +0700

    Refactor the timebased charge model to support group-based pricing

--- app/services/charges/charge_models/timebased_service.rb
+++ app/services/charges/charge_models/timebased_service.rb
@@ -24,11 +24,25 @@ module Charges
       end
 
       def per_package_unit_amount
-        @per_package_unit_amount ||= BigDecimal(properties['amount'])
+        @per_package_unit_amount ||= if is_in_group_charge?
+          per_group_package_unit_amount
+        else
+          BigDecimal(properties['amount'])
+        end
       end
 
       def paid_units
         @paid_units ||= units
+      end
+
+      private
+
+      def is_in_group_charge?
+        charge.charge_group_id.present?
+      end
+
+      def per_group_package_unit_amount
+        @per_group_package_unit_amount ||= BigDecimal(charge.charge_group.properties['amount'])
       end
     end
   end
